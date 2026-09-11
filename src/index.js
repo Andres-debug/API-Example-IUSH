a0da9d8e7d78cb82a67c92f49774cea634a8cb01
Andres Diaz <60768402+Andres-debug@users.noreply.github.com>
2026-09-11 09:43:01 -0500

    Update API message to reflect remote status

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = Number.parseInt(process.env.PORT || "3000", 10);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "API activa pero local" });
+  res.json({ message: "API activa pero local desde el remoto" });
 });
 
 app.use("/users", usersRouter);
